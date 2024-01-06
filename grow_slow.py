cbfff4775593cd2c200f0896262bf3f475bc77b7
Tom <tombrandisicloud@gmail.com>
2024-01-06 14:00:33 +0000

    save each photo taken with a timestamp
    each photo is saved with a name that includes the date to avoid overwriting

--- grow_slow.py
+++ grow_slow.py
@@ -11,13 +11,20 @@ CONSUMER_SECRET = 'your consumer secret here'
 ACCESS_KEY = 'your access key here'
 ACCESS_SECRET = 'your access secret here'
 
+# this gets the date from the command line
+# to overwrite the picture each day use
+# date = ""
+# instead
+date = os.system("date -I")
+
+filename = f"plantpic-{date}.jpg"
 
 # this runs the following script from the command line that takes the photo and saves it
 # it will only work for USB webcams,
 # you'll have to do something different if you're using a pi-cam
-os.system ("fswebcam -d/dev/video0 -r1280x960 --no-banner plantpic.jpg")
+os.system (f"fswebcam -d/dev/video0 -r1280x960 --no-banner {filename}")
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
-photo = open('plantpic.jpg','rb')
+photo = open(filename,'rb')
 image_ids = api.upload_media(media=photo)
 api.update_status(status='', media_ids=image_ids['media_id'])
